0c404028e9ca53ab2b689b2e249ecb0c38d78065
Christian <j.christian.marini+git@gmail.com>
2020-03-19 16:12:33 -0400

    Removed unused input handling from the Die themselves. It was moved to an single input field

--- client/src/components/Die/Die.js
+++ client/src/components/Die/Die.js
@@ -13,40 +13,6 @@ export default class Die extends React.PureComponent {
         };
     }
 
-    validateDie(string, increment) {
-        console.log(`validateDie: die=${this.props.idx} str='${string}' inc=${increment}`);
-        this.setState({ value: string, valid: string.length > 0 ? true : false });
-    }
-
-    handleChange(event) {
-        let str = event.target.value;
-        let cur = this.state.value;
-        let next = 1;
-        if (str.length > cur.length) {
-            if (str.substr(0, cur.length) === cur) {
-                str = str.substr(cur.length);
-            }
-        }
-        else {
-            next = -1;
-        }
-        let char = toComboDie(str);
-        if (char.length === 0) {
-            next = 0;
-        }
-
-        this.validateDie(char, next);
-    }
-
-    handleKeyUp(event) {
-        if (event.key === "Backspace" && event.target.value === "") {
-            this.validateDie("", -1);
-            this.setState((prevState, props) => {
-                return { valid: false }
-            });
-        }
-    }
-
     render() {
         let valid = this.props.value ? "valid" : "invalid";
         let character = this.props.value.length <= 0 ? "\u00a0" : toComboDie(this.props.value);
